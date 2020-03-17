2e84f9734e91a947ab98f4592e6eecdba02a6d1c
Matthias <matthias.gomolka@posteo.de>
2020-03-17 09:33:54 +0100

    message_arguments() should now work for all types of by, by_to_char() moved from utils to status_messages

--- R/status_messages.R
+++ R/status_messages.R
@@ -8,16 +8,18 @@ message_options <- function() {
     )
 }
 
+#' @importFrom methods hasArg
 message_arguments <- function(id_var, val_var = NULL, by = NULL) {
     msg_id_var  <- paste0("id_var: ", id_var)
 
-    if (!is.null(val_var)) {
+    if (methods::hasArg(val_var)) {
         msg_val_var <- paste0(" | val_var: ", val_var)
     } else {
         msg_val_var <- ""
     }
-    if (!is.null(by)) {
-        msg_by <- paste0(" | by: ", by_to_character(substitute(by)))
+    if (methods::hasArg(by)) {
+        by_sub <- substitute(by, env = parent.frame())
+        msg_by <- paste0(" | by: ", by_to_char(by_sub))
     } else {
         msg_by <- ""
     }
@@ -29,4 +31,17 @@ message_arguments <- function(id_var, val_var = NULL, by = NULL) {
         msg_by,
         " ]"
     )
+}
+
+by_to_char <- function(by) {
+    str <- as.character(by)
+    if (length(str) == 1L) {
+        return(gsub(",", ", ", fixed = TRUE, str))
+    }
+
+    if (grepl("^(:|>|<|=)", str[1L])) {
+        return(paste(str[2L], str[1L], str[3L]))
+    } else {
+        return(paste(str[-1], collapse = ", "))
+    }
 }
