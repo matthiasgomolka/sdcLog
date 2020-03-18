0d462b13b938e2557d0a6c54269b2c7a3b5fa98d
Matthias <matthias.gomolka@posteo.de>
2020-03-18 15:50:55 +0100

    utils improvements

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,22 @@
-# make package aware of data.table usage
+# pure technical settings
 .datatable.aware <- TRUE
+utils::globalVariables(".")
 
 #' @importFrom checkmate assert_data_frame assert_string assert check_class
+#' @importFrom methods hasArg
 check_args <- function(data, id_var, val_var = NULL, by = NULL) {
     checkmate::assert_data_frame(data)
     checkmate::assert_string(id_var)
     if (!is.null(val_var)) {
         checkmate::assert_string(val_var)
     }
-    checkmate::assert(
-        combine = "or",
-        .var.name = "by",
-        checkmate::check_class(substitute(by), "name"),
-        checkmate::check_class(substitute(by), "call"),
-        checkmate::check_class(substitute(by), "character"),
-    )
+    if (methods::hasArg(by)) {
+        checkmate::assert(
+            combine = "or",
+            .var.name = "by",
+            checkmate::check_class(substitute(by), "name"),
+            checkmate::check_class(substitute(by), "call"),
+            checkmate::check_class(substitute(by), "character"),
+        )
+    }
 }
