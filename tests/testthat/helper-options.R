a1b5826bd6f9269148480bd34e7a8c43b09e7250
Matthias <matthias.gomolka@posteo.de>
2020-11-09 16:19:15 +0100

    cleanup

--- tests/testthat/helper-options.R
+++ tests/testthat/helper-options.R
@@ -1,3 +1,8 @@
+# reset sdcLog options
+options(sdc.n_ids = NULL)
+options(sdc.n_ids_dominance = NULL)
+options(sdc.share_dominance = NULL)
+
 # set data.table options
 options(datatable.print.class = FALSE)
 options(datatable.print.keys = FALSE)
