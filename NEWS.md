689573a5a801a0bcd3cfb53ff15d10e8ab60f167
Matthias <matthias.gomolka@posteo.de>
2020-12-16 22:03:24 +0100

    check_dominance() improved

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,15 @@
-# sdc 0.2.0
+# sdcLog 0.2.0
+
+### Features
 
 * support for interaction terms added to `sdc_model()`
+* performance improvement for `check_dominance()` (and therefore in `sdc_descriptives()` and `sdc_extreme()`)
 
-# sdc 0.1.0 (first CRAN release)
+### Bug Fixes
+
+* `check_dominance()` now handles negative values correctly (as `abs()`)
+
+
+# sdcLog 0.1.0 (first CRAN release)
 
 * Added a `NEWS.md` file to track changes to the package.
--- R/check_dominance.R
+++ R/check_dominance.R
@@ -2,18 +2,17 @@
 #' @inheritParams common_arguments
 check_dominance <- function(data, id_var, val_var, by = NULL) {
   substitute(
-    data[!is.na(get(val_var)),
-      # base::sum needed to avoid error with gsum
-      .SD[,
-        .(agg_val_var = base::sum(.SD)),
-        .SDcols = val_var,
-        by = id_var
-      ],
+    data[
+      i = !is.na(get(val_var)),
+      j = .SD[, .(agg_val_var = sum(abs(get(val_var)))), by = id_var],
       keyby = by
-    ][order(-agg_val_var),
-      .(value_share = cumsum(agg_val_var) / sum(agg_val_var)),
+    ][
+      i = order(-agg_val_var),
+      j = .(value_share = cumsum(agg_val_var) / sum(agg_val_var)),
       keyby = by
-    ][, .SD[getOption("sdc.n_ids_dominance", 2L)], keyby = by
+    ][
+      j = .SD[getOption("sdc.n_ids_dominance", 2L)],
+      keyby = by
     ][order(-value_share)]
   )
 }
